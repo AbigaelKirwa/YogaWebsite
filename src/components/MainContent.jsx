0bce1847b89939d4afe6d9b8d18a58c3010a1cfa
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2023-09-16 11:08:29 +0300

    removed a tags

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,9 +3,9 @@ import MainImg from '../images/desktop-view.png'
 
 function MainContent(){
   return(
-    <div id='main-overall' className='flex flex-row justify-between'>
-        <div id="words" className='mt-20 ml-10 w-2/3'>
-            <h1 className='text-center text-5xl text-[#D86E32]'>Tranquil Yoga Studio</h1>
+    <div id='main-overall' className='flex flex-row justify-between max-[841px]:flex-col-reverse'>
+        <div id="words" className='mt-20 ml-10 w-2/3 max-[1039px]:w-full'>
+            <h1 className='text-center text-5xl text-[#D86E32] max-[1039px]:text-4xl'>Tranquil Yoga Studio</h1>
             <p className='mt-10  text-justify text-[#0C2D29] font-medium leading-[50px] px-5'>
                 Discover a world of inner peace and physical well-being 
                 through the transformative practice of yoga. Our studio 
@@ -15,8 +15,8 @@ function MainContent(){
                 <button className='w-full h-16  rounded-full text-xl font-semibold text-white bg-gradient-to-r from-[#D7763F] to-[#D86E32]'>Learn More</button>
             </div>
         </div>
-        <div id="main-pic" className='mt-[-80px] ml-28'>
-            <img src={MainImg} alt="website main image"  className='w-[90%]'/>
+        <div id="main-pic" className='mt-[-80px] flex justify-center max-[1130px]:mt-[-65px] max-[1076px]:mt-[-30px] max-[1039px]:mt-[10px] max-[975px]:mt-[40px] max-[841px]:ml-0 max-[841px]:mt-0'>
+            <img src={MainImg} alt="website main"  className='w-[80%] max-[841px]:w-[50%]'/>
         </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,22 +5,16 @@ function Navbar(){
 
   return(
     <div>
-    <div id='desktop-view' className='flex flex-row justify-between max-[841px]:hidden'>
+    <div id='desktop-view' className='flex flex-row justify-between'>
       <div id="navbar">
         {/* this is the menu that is displayed in full screen */}
         <ul className="ml-10 mt-10 flex flex-row gap-20">
-          <li className="w-32 text-xl text-center font-semibold h-12 pt-1.5 text-white bg-gradient-to-r from-[#800096] to-[#C772E4] rounded-full"><div className="home-btn"><a href="#">Home</a></div></li>
-          <li className="mt-2 text-xl font-semibold text-[#733D64]"><a href="#">About</a></li>
-          <li className="mt-2 text-xl font-semibold text-[#733D64]"><a href="#">Services</a></li>
-          <li className="mt-2 text-xl font-semibold text-[#733D64]"><a href="#">Contact</a></li>
+          <li className="w-32 text-xl text-center font-semibold h-12 pt-1.5 text-white bg-gradient-to-r from-[#800096] to-[#C772E4] rounded-full"><div className="home-btn">Home</div></li>
+          <li className="mt-2 text-xl font-semibold text-[#733D64]">About</li>
+          <li className="mt-2 text-xl font-semibold text-[#733D64]">Services</li>
+          <li className="mt-2 text-xl font-semibold text-[#733D64]">Contact</li>
         </ul>
       </div>
-      <div id='logo' className='mr-16 mt-7'>
-        <img src={Logo} alt='website-logo' className='w-16'></img>
-      </div>
-    </div>
-    <div id="mobile-view" className='flex flex-row justify-between min-[841px]:hidden'>
-
       <div id='logo' className='mr-16 mt-7'>
         <img src={Logo} alt='website-logo' className='w-16'></img>
       </div>
