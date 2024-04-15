5d8861bd6c4b36a687be3cd886b26196bf7f515f
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-04-15 18:06:54 +0300

    fix(home):added image size and changed words

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,19 +3,17 @@ import MainImg from '../images/desktop-view.png'
 function MainContent(){
   return(
     <div id='main-overall' className='flex flex-row justify-between max-[841px]:flex-col-reverse'>
-        <div id="words" className='mt-20 ml-10 w-2/3 max-[1039px]:w-full max-[841px]:ml-0 max-[841px]:px-10 max-[841px]:mt-10'>
+        <div id="words" className='mt-16 ml-10 w-2/3 max-[1039px]:w-full max-[841px]:ml-0 max-[841px]:px-10 max-[841px]:mt-10'>
             <h1 className='text-center text-5xl text-[#D86E32] max-[1039px]:text-4xl max-[841px]:text-5xl max-[490px]:text-3xl max-[346px]:leading-[50px]'>Tranquil Yoga Studio</h1>
             <p className='text-base mt-10 mr-10  text-justify text-[#0C2D29] font-medium leading-[50px] px-5 max-[841px]:mr-0 max-[490px]:px-2 max-[358px]:px-0 max-[346px]:leading-[40px]'>
-                Discover a world of inner peace and physical well-being 
-                through the transformative practice of yoga. Our studio 
-                is a sanctuary where you can escape the stresses of everyday life.
+            In our nurturing environment, experienced instructors will guide you through poses that cultivate strength, flexibility, and mindfulness. Join our community and embark on a journey towards holistic wellness, fitness, and a balanced mind-body-spirit connection.
             </p>
             <div className='mx-20 mt-12 max-[490px]:mx-10 max-[377px]:mx-5 max-[841px]:mb-10'>
                 <button className='w-full h-16 rounded-full text-xl font-semibold text-white bg-gradient-to-r from-[#D7763F] to-[#D86E32] shadow-2xl shadow-[#9a9a9a]'>Learn More</button>
             </div>
         </div>
         <div id="main-pic" className='mt-[-40px] flex justify-center max-[1130px]:mt-[-35px] max-[1076px]:mt-[10px] max-[1039px]:mt-[10px] max-[975px]:mt-[40px] max-[841px]:ml-0 max-[841px]:mt-0'>
-            <img src={MainImg} alt="website main"  className='w-[70%] max-[841px]:w-[50%] max-[500px]:w-[70%]'/>
+            <img src={MainImg} alt="website main"  className='w-[70%] max-[1000px]:w-[100%] max-[841px]:w-[50%] max-[500px]:w-[70%]'/>
         </div>
     </div>
   )
